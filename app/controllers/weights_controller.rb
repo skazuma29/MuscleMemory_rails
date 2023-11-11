6220278b8ca138644c934e3b7bb6e11ace44c02f
sato <kpezurin@gmail.com>
2023-11-11 00:06:52 +0000

    createアクションを実装し体重の登録を可能に。

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -5,4 +5,20 @@ class WeightsController < ApplicationController
     def new
         @weight = Weight.new
     end
+    
+    def create
+      @weight = Weight.new(weight_params)
+      
+      if @weight.save
+        redirect_to weights_path,notice: '体重が記録されました'
+      else
+        puts @weight.errors.full_messages
+        render :new
+      end
+    end
+    
+    private
+     def weight_params
+       params.require(:weight).permit(:weight, :date, :user_id)
+     end
 end
